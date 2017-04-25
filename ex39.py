5ad5a20965eb1c8c5549a6f5899f76d0afc3e411
Anayatc <Sincity9490>
2017-04-25 12:12:50 +0100

    Dictionaries

--- ex39.py
+++ ex39.py
@@ -1,19 +1,12 @@
 states = {
     'Oregon': 'OR',
     'Florida': 'FL',
-    'Californi':'CA',
+    'California':'CA',
     'New York': 'NY',
     'Michigan': 'MI'
 }
 
-cities = {
-    'CA': 'San Francisco',
-    'MI': 'Detroit',
-    'FL': 'Jacksonville'
-}
-
-cities['NY'] = 'New York'
-cities['OR'] = 'Portland'
+cities = {'CA': 'San Francisco', 'MI': 'Detroit', 'FL': 'Jacksonville', 'NY': 'New York', 'OR': 'Portland'}
 
 print('-' * 10)
 print("NY State has: ", cities['NY'])
@@ -26,3 +19,24 @@ print("Florida's abbreviation is: ", states['Florida'])
 print('-' * 10)
 print("Michigan has ", cities[states['Michigan']])
 print("Florida has ", cities[states['Florida']])
+
+print('-' * 10)
+for state, abbrev in states.items():
+    print("%s is abbreviated %s" % (state, abbrev))
+
+print('-' * 10)
+for abbrev, city in cities.items():
+    print("%s has the city %s" % (abbrev, city))
+
+print('-' * 10)
+for state, abbrev in states.items():
+    print("%s state is abbreviated %s and has city %s" % (state, abbrev, cities[abbrev]))
+
+print('-' * 10)
+state = states.get('Texas', None)
+
+if not state:
+    print("Sorry, no Texas.")
+
+city = cities.get('TX', 'Does Not Exist')
+print("The city fo the state 'TX' is: %s" % city)
