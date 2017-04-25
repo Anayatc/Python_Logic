da411d8e0659b42fb5ecfc356880817841989b76
Anayatc <Sincity9490>
2017-04-25 11:53:10 +0100

    Dictionaries

---
+++ ex39.py
@@ -0,0 +1,22 @@
+states = {
+    'Oregon': 'OR',
+    'Folrida': 'FL',
+    'Californi':'CA',
+    'New York': 'NY',
+    'Michigan': 'MI'
+}
+
+cities = {
+    'CA': 'San Francisco',
+    'MI': 'Detroit',
+    'FL': 'Jacksonville'
+}
+
+cities['NY'] = 'New York'
+cities['OR'] = 'Portland'
+
+print('-' * 10)
+print("NY State has: ", cities['NY'])
+print("OR State has: ", cities['OR'])
+
+print('-' * 10)
